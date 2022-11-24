7a7b7cbab45e3bca9d863ee0cc1ff89c1f381595
Xaephare <100387237+Xaephare@users.noreply.github.com>
2022-11-24 16:29:58 +1300

    Fixed formatting

--- src/commands/Commands.ts
+++ src/commands/Commands.ts
@@ -1,5 +1,5 @@
-import {Command} from "../structures/Command";
-import {Ping} from "./info/ping";
-import {Rotation} from "./info/rotation";
+import { Command } from "../structures/Command";
+import { Ping } from "./info/ping";
+import { Rotation } from "./info/rotation";
 
 export const Commands: Command[] = [Ping, Rotation];
--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,15 +1,15 @@
-import {Command} from "src/structures/Command";
-import {Client, CommandInteraction} from "discord.js";
+import { Command } from "src/structures/Command";
+import { Client, CommandInteraction } from "discord.js";
 
 export const Ping: Command = {
-    name: "ping",
-    description: "Displays bot ping.",
-    run: async (client: Client, interaction: CommandInteraction) => {
-        const content = `${interaction.client.ws.ping}ms`
+  name: "ping",
+  description: "Displays bot ping.",
+  run: async (client: Client, interaction: CommandInteraction) => {
+    const content = `${interaction.client.ws.ping}ms`;
 
-        await interaction.reply({
-            ephemeral: true,
-            content
-        })
-    }
-}
+    await interaction.reply({
+      ephemeral: true,
+      content,
+    });
+  },
+};
